a8f59d91d67012ca8828ec756487ba1f48f02ca0
bunyod <naimov.bunyod2021@gmail.com>
2024-05-13 12:43:14 +0500

    update

--- bot.py
+++ bot.py
@@ -6,9 +6,9 @@ from aiogram.fsm.context import FSMContext
 from dotenv import load_dotenv
 
 from commands import commands
-from db import db_get_all_products, db_insert_product, insert_user, insert_orders, db_get_all_orders, \
-    db_get_all_favorites, db_insert_favorites
-from keyboards import kb, ikb, buy_ikb
+from db import db_get_all_products, db_insert_product, insert_user, db_insert_orders, db_get_all_orders, \
+    db_get_all_favorites, db_insert_favorites, db_get_user, db_delete_order, db_delete_favorite
+from keyboards import kb, ikb, buy_ikb, admin_ikb, delete_order_ikb, delete_favorites_ikb
 from states import ProductStatesGroup, UserRegisterStatesGroup
 
 # from aiogram.client.session.aiohttp import AiohttpSession
@@ -22,6 +22,7 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
+
 @dp.message(CommandStart())
 async def cmd_start(message: types.Message):
     await message.answer("Assalomu Aleykum!\n Do'konimizga xush kelibsiz!\n"
@@ -30,7 +31,12 @@ async def cmd_start(message: types.Message):
 
 @dp.message(Command('products'))
 async def cmd_products(message: types.Message):
-    await message.answer("Mahsulotlarni boshqarish!", reply_markup=ikb)
+    user_id = message.from_user.id
+    user = await db_get_user(user_id)
+    if user[-1] == 1:
+        await message.answer("Mahsulotlarni boshqarish!", reply_markup=admin_ikb)
+    if user[-1] != 1 or user is None:
+        await message.answer("Mahsulotlarni boshqarish!", reply_markup=ikb)
 
 
 @dp.message(Command('registration'))
@@ -43,10 +49,15 @@ async def cmd_registration(message: types.Message, state: FSMContext):
 async def cmd_orders(message: types.Message):
     user_id = message.from_user.id
     user, products = await db_get_all_orders(int(user_id))
+    if len(products) == 0:
+        await message.answer("Savatchada mahsulot yo'q")
     for product in products:
-        await message.answer(text=f"{user[1]}\n"
-                                  f"Zakazlaringiz\n"
-                                  f"Nomi: {product[1]}")
+        if product is not None:
+            await message.answer_photo(photo=product[3], caption=f"{user[1]}\n"
+                                                                 f"Zakazlaringiz\n"
+                                                                 f"Nomi: {product[1]}\n"
+                                                                 f"Product id: {product[0]}",
+                                       reply_markup=delete_order_ikb)
 
 
 @dp.message(Command('favorites'))
@@ -56,9 +67,11 @@ async def cmd_favorites(message: types.Message):
     if not products:
         await message.answer(text='Not favorites')
     for product in products:
-        await message.answer(text=f"{message.from_user.full_name}\n"
-                                  f"Zakazlaringiz\n"
-                                  f"Nomi: {product[1]}")
+        await message.answer_photo(photo=product[3], caption=f"{message.from_user.full_name}\n"
+                                                             f"Zakazlaringiz\n"
+                                                             f"Nomi: {product[1]}\n"
+                                                             f"Product id: {product[0]}",
+                                   reply_markup=delete_favorites_ikb)
 
 
 @dp.message(UserRegisterStatesGroup.full_name)
@@ -76,29 +89,48 @@ async def user_phone(message: types.Message, state: FSMContext):
     await message.answer("Ro'yxatdan o'tdingiz! ")
 
 
-@dp.callback_query(F.data == 'sevimlilar')
-async def sevimlilar(callback: types.CallbackQuery):
-    user_id = callback.from_user.id
-    product_id = callback.message.caption.split('id:')[-1]
-    await db_insert_favorites(user_id, int(product_id))
-    await callback.message.answer(text="Sevimlilarga qo'shildi!")
-
-
 @dp.callback_query(F.data == 'savatchaga')
 async def savatchaga(call: types.CallbackQuery):
     product_id = int(call.message.caption.split('id:')[-1])
     user_id = call.from_user.id
-    await insert_orders(product_id, user_id)
-    await call.message.answer("Mahsulot savatchaga joylandi!")
+    msg = await db_insert_orders(product_id, user_id)
+    await call.message.answer(msg)
+
+
+@dp.callback_query(F.data == 'delete_order')
+async def delete_order(call: types.CallbackQuery):
+    product_id = call.message.caption.split('id:')[-1]
+    await call.message.delete()
+    await db_delete_order(product_id)
+    await call.message.answer("Savatchadan o'chirildi")
+
+
+@dp.callback_query(F.data == 'sevimlilar')
+async def sevimlilar(callback: types.CallbackQuery):
+    user_id = callback.from_user.id
+    product_id = callback.message.caption.split('id:')[-1]
+    msg = await db_insert_favorites(user_id, int(product_id))
+    await callback.message.answer(text=msg)
+
+
+@dp.callback_query(F.data == 'delete_favorite')
+async def delete_order(call: types.CallbackQuery):
+    product_id = call.message.caption.split('id:')[-1]
+    await call.message.delete()
+    await db_delete_favorite(product_id)
+    await call.message.answer("Sevimlilardan o'chirildi")
+
+
 
 
 @dp.callback_query(F.data == 'get_all_product')
 async def get_all_product(call: types.CallbackQuery):
-    product = await db_get_all_products()
+    products = await db_get_all_products()
     await call.message.delete()
-    if not product:
+    if not products:
         await call.message.answer("Mahsulot mavjud emas!")
-    for product in product:
+    for product in products:
+        print(product)
         await call.message.answer_photo(photo=product[3],
                                         caption=f"Mahsulot nomi: {product[1]}\n"
                                                 f"Mahsulot narxi: {product[2]}\n"
@@ -130,12 +162,18 @@ async def create_product_price(message: types.Message, state: FSMContext):
 async def create_product_photo(message: types.Message, state: FSMContext):
     await state.update_data(photo=message.photo[-1].file_id)
     data = await state.get_data()
-    await message.answer("Mahsulot yaratildi!")
-    await db_insert_product(data['title'], data['price'], data['photo'])
+    user = await db_get_user(message.from_user.id)
+    if user[-1] == 1:
+
+        await message.answer("Mahsulot yaratildi!")
+
+        await db_insert_product(data['title'], data['price'], data['photo'])
+    else:
+        await message.answer("Admin Emassiz! Chiterlik qilmang!")
 
 
 async def main():
-    print("Started.....")
+    print("Started......")
     await bot.set_my_commands(commands=commands)
     await dp.start_polling(bot)
 
